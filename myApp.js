cf28d886a465d711bf7a97c993d1846a2def1456
Vernon Williams <voxenn@gmail.com>
2022-01-24 07:30:52 -0600

    Implement find by id

--- myApp.js
+++ myApp.js
@@ -49,7 +49,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+    Person.findById(personId, (err, person) => {
+        if(err) console.err();
+            done(null, person);
+    });
 };
 
 const findEditThenSave = (personId, done) => {
