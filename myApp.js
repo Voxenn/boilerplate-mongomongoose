e793e9d19373a8f98b23dd3c44e0c3954975d6c1
Vernon Williams <voxenn@gmail.com>
2022-01-24 08:49:34 -0600

    Correct chain methods

--- myApp.js
+++ myApp.js
@@ -103,8 +103,12 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-  Person.find({favoriteFoods: foodToSearch}).sort().limit(2).select(name, favoriteFoods).exec((err, people) => {
-    if(err) return console.err();
+  Person.find({favoriteFoods: foodToSearch})
+    .sort({name: 1})
+    .limit(2)
+    .select({name: 1, age: 0, favoriteFoods: 1)
+    .exec((err, people) => {
+        if(err) return console.err();
         done(null, people);
   });
 };
